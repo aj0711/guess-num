30d86e75171cdf861f296f911a258390fa253d19
Clark <ck0913660913@gmail.com>
2021-03-29 16:14:01 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,6 +1,10 @@
 import random #載入random
+start = input('Start Number? ')
+end = input('End Number? ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 10) #random的random int隨機整數(從1到100)
+r = random.randint(start, end) #random的random int隨機整數(從1到100)
 count = 0
 while True:
 	count += 1 # count = count + 1
