ac830f4ad80c5a778174220daf0beb1267dbd741
Clark <ck0913660913@gmail.com>
2021-03-29 16:08:42 +0800

    add count

--- guess.py
+++ guess.py
@@ -1,13 +1,17 @@
 import random #載入random
 
 r = random.randint(1, 10) #random的random int隨機整數(從1到100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('Guess? ')
 	num = int(num)
 	if num == r: 
 		print('U R Right')
+		print('This is', count, '次')
 		break
-	elif num < r:
+	elif num > r:
 		print('Ans is smaller ')
 	else:
 		print('Ans is bigger')
+	print('This is', count, '次')
